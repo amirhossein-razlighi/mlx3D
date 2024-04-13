45ab4f4053e6bca250b6d44cf3b07358a0100146
AmirHossein-nr <arazlighi@gmail.com>
2024-04-13 11:40:10 +0330

    update config for docs

--- documentation/source/conf.py
+++ documentation/source/conf.py
@@ -10,14 +10,15 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
-# import os
-# import sys
-# sys.path.insert(0, os.path.abspath('.'))
+import os
+import sys
+
+sys.path.insert(0, os.path.abspath("."))
 
 
 # -- Project information -----------------------------------------------------
 
-project = "mlx3d"
+project = "mlx3D"
 copyright = "2024, AmirHossein Razlighi"
 author = "AmirHossein Razlighi"
 
@@ -29,10 +30,27 @@ author = "AmirHossein Razlighi"
 # ones.
 extensions = [
     "myst_parser",
+    "sphinx.ext.autodoc",
+    "sphinx.ext.autosummary",
+    "sphinx.ext.intersphinx",
+    "sphinx.ext.napoleon",
 ]
 
-# Add any paths that contain templates here, relative to this directory.
+python_use_unqualified_type_names = True
+autosummary_generate = True
+
+intersphinx_mapping = {
+    "python": ("https://docs.python.org/3", None),
+    "numpy": ("https://numpy.org/doc/stable/", None),
+}
+
 templates_path = ["_templates"]
+html_static_path = ["_static"]
+source_suffix = ".rst"
+main_doc = "index"
+highlight_language = "python"
+pygments_style = "sphinx"
+add_module_names = False
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
@@ -45,7 +63,20 @@ exclude_patterns = []
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = "alabaster"
+html_theme = "sphinx_book_theme"
+
+html_theme_options = {
+    "show_toc_level": 2,
+    "repository_url": "https://amirhossein-razlighi.github.io/mlx3D/",
+    "use_repository_button": True,
+    "navigation_with_keys": False,
+    "logo": {
+        "image_light": "Logo.jpeg",
+        "image_dark": "Logo.jpeg",
+    },
+}
+
+htmlhelp_basename = "mlx3D_doc"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
